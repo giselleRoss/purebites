26e35b21b8cc1d7f4994e42383df0f6643bc4b53
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-04-19 08:58:29 -0400

    cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   const [featuredClicked, setFeaturedClicked] = useState(false);
   const [showRecipeList, setShowRecipeList] = useState(false); 
 
-  const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = import.meta.VITE_BACKEND_URL;
   
   const getSingleRecipe = async (id) => {
     const res = await fetch(`/api/recipes/${id}`);
@@ -29,7 +29,7 @@ function App() {
 
   const getRecipes = async () => {
     try {
-      const res = await fetch(`${BASE_URL}/api/recipes`);
+      const res = await fetch(`https://purebites-1.onrender.com/api/recipes`);
       console.log(res)
       const data = await res.json();
       console.log("This is the data ", data)
@@ -42,7 +42,7 @@ console.log("These are the recipes;", recipes)
   
   const getFeaturedRecipes = async () => {
     try {
-      const res = await fetch(`${BASE_URL}/api/recipes/:id/featured`);
+      const res = await fetch(`https://purebites-1.onrender.com/api/recipes/:id/featured`);
       console.log(res)
       const data = await res.json();
       console.log("This is the data ", data)
@@ -60,7 +60,7 @@ console.log("These are the recipes;", recipes)
 
   const createRecipe = async (recipe) => {
     try {
-      const res = await fetch(`${BASE_URL}/api/recipes`, {
+      const res = await fetch(`https://purebites-1.onrender.com/api/recipes`, {
         method: "POST", // or 'PUT'
         headers: {
           "Content-Type": "application/json",
