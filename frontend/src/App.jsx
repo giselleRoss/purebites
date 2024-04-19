1a52e21915fc52a192a17563f20dfb2d127c043d
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-04-19 08:27:25 -0400

    Added render database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   const [featuredClicked, setFeaturedClicked] = useState(false);
   const [showRecipeList, setShowRecipeList] = useState(false); 
 
+  const BASE_URL = process.env.BACKEND_URL;
   
   const getSingleRecipe = async (id) => {
     const res = await fetch(`/api/recipes/${id}`);
@@ -28,7 +29,7 @@ function App() {
 
   const getRecipes = async () => {
     try {
-      const res = await fetch(`/api/recipes`);
+      const res = await fetch(`${BASE_URL}/api/recipes`);
       console.log(res)
       const data = await res.json();
       console.log("This is the data ", data)
@@ -41,7 +42,7 @@ console.log("These are the recipes;", recipes)
   
   const getFeaturedRecipes = async () => {
     try {
-      const res = await fetch(`/api/recipes/:id/featured`);
+      const res = await fetch(`${BASE_URL}/api/recipes/:id/featured`);
       console.log(res)
       const data = await res.json();
       console.log("This is the data ", data)
@@ -59,7 +60,7 @@ console.log("These are the recipes;", recipes)
 
   const createRecipe = async (recipe) => {
     try {
-      const res = await fetch(`/api/recipes`, {
+      const res = await fetch(`${BASE_URL}/api/recipes`, {
         method: "POST", // or 'PUT'
         headers: {
           "Content-Type": "application/json",
